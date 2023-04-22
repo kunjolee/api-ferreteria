1022ad25032dabff0ad6188ffac53a0370433873
DGUGLAS <moratayadouglas800@gmail.com>
2023-04-21 18:34:47 -0600

    Tabla Terminada

--- Controllers/clienteController.cs
+++ Controllers/clienteController.cs
@@ -1,6 +1,8 @@
 ﻿using api_ferreteria.Models.article;
+using api_ferreteria.Models.Client;
 using Microsoft.AspNetCore.Mvc;
-//using static api_ferreteria.Models.article.csArticleStructure;
+using System.Reflection;
+using static api_ferreteria.Models.Client.csClienteStructure;
 
 namespace api_ferreteria.Controllers
 {
@@ -12,13 +14,44 @@ namespace api_ferreteria.Controllers
     {
 
         [HttpPost]
-        [Route("saveClient")]
-        public dynamic saveArticle()
+        [Route("saveCliente")]
+        public dynamic saveArticle(requesClient model)
         {
-            return Ok(new csCliente().;
+            return Ok(new csCliente().insertClient(model.nombre, model.apellido, model.DPI));
 
         }
 
+
+        [HttpPost]
+        [Route("updateClient")]
+        public dynamic updateClient(requesClient model)
+        {
+            return Ok(new csCliente().updateClient(model.idCliente, model.nombre, model.apellido, model.DPI));
+        }
+
+
+        [HttpPost]
+        [Route("deleteArticle")]
+        public dynamic deleteArticle(requestDeleteCliente model)
+        {
+            return Ok(new csArticle().deleteArticle(model.idArticulo));
+
+        }
+
+        [HttpGet]
+        [Route("listClient")]
+        public dynamic listArticles()
+        {
+            return Ok(new csCliente().listClient());
+        }
+
+        [HttpGet]
+        [Route("listClientById")]
+        public dynamic listArticlesById(string IdCliente)
+        {
+
+            return Ok(new csArticle().listArticlesById(Int32.Parse(IdCliente)));
+        }
 
 
     }
--- Models/Client/csClienteStructure.cs
+++ Models/Client/csClienteStructure.cs
@@ -2,5 +2,25 @@
 {
     public class csClienteStructure
     {
+        public class requesClient
+        {
+            public int idCliente { get; set; }
+            public string nombre { get; set; }
+            public string apellido { get; set; }
+            public int DPI { get; set; }
+
+        }
+
+        public class responseClient
+        {
+            public int response { get; set; } //-> 0 | 1
+            public string response_description { get; set; } //-> message success | failed
+        }
+
+        public class requestDeleteCliente
+        {
+            public int idArticulo { get; set; }
+        }
+
     }
 }
